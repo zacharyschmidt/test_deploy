208b6f3f5334c4b86987ee4b79c2001199243968
Zachary Schmidt <zacharym.schmidt@gmail.com>
2021-05-04 17:44:51 -0700

    working on excel download bug

--- server/src/download/download.controller.ts
+++ server/src/download/download.controller.ts
@@ -1,15 +1,15 @@
-import {Controller, Get, Header, HttpCode, Query, Req, Res} from '@nestjs/common';
-import {Response, Request} from 'express';
+import { Controller, Get, Header, HttpCode, Query, Req, Res } from '@nestjs/common';
+import { Response, Request } from 'express';
 
-import {CategoryService} from '../categories/category.service';
-import {SeriesService} from '../series/series.service';
+import { CategoryService } from '../categories/category.service';
+import { SeriesService } from '../series/series.service';
 import { PaginationDto } from '../categories/dto/Pagination.dto';
 
 import { createReadStream } from 'fs';
 import * as Excel from 'exceljs';
 const createAEORIS = (title: string, year: string) => {
     return (
-`TY  - DATA
+        `TY  - DATA
 KW  - Annual Energy Outlook AEO EIA Energy Information Administration long term forecast projection united states production consumption trade electricity petroleum natural gas coal nuclear renewable hydroelectric wind solar
 N1  - The Annual Energy Outlook (AEO) from EIA.gov provides long term forecasts (25 years) of U.S. energy production, consumption, and trade for the United Stated of electricity, petroleum, natural gas, coal, nuclear, and renewable sources.
 PB  - U.S. Energy Information Administration
@@ -32,46 +32,47 @@ ER  -`
 }
 @Controller('download')
 export class DownloadController {
-    constructor(private categoryService: CategoryService, 
-                private seriesService: SeriesService) {}
+    constructor(private categoryService: CategoryService,
+        private seriesService: SeriesService) { }
 
 
     @Get('RIS')
     @HttpCode(201)
     @Header('Content-Type', 'text/plain')
     @Header('Content-Disposition', 'attachment; filename=test.RIS')
-    async getRISDownload(@Res() response: Response, 
-                     @Query('category_ID') category_ID: number) {
-        const cat = await this.categoryService.getCategorybyID(category_ID); 
+    async getRISDownload(@Res() response: Response,
+        @Query('category_ID') category_ID: number) {
+        const cat = await this.categoryService.getCategorybyID(category_ID);
         console.log(cat)
         const year = cat.dataset_name.slice(22)
-        if ( cat.dataset_name.includes('Annual Energy Outlook')) {
-        return response.send(createAEORIS(cat.dataset_name, year)) }
+        if (cat.dataset_name.includes('Annual Energy Outlook')) {
+            return response.send(createAEORIS(cat.dataset_name, year))
+        }
         return 0
-                     }
+    }
 
     @Get('excel')
     @HttpCode(201)
     @Header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     @Header('Content-Disposition', 'attachment; filename=test.xlsx')
-    async getExcelDownload(@Res() response: Response, 
-                     @Query('category_ID') category_ID: number,
-                     @Query('custom_flag') custom_flag: string,
-                     @Query() paginationDto: PaginationDto) {
+    async getExcelDownload(@Res() response: Response,
+        @Query('category_ID') category_ID: number,
+        @Query('custom_flag') custom_flag: string,
+        @Query() paginationDto: PaginationDto) {
 
         console.log("IN GET EXCEL")
-        let cat; 
+        let cat;
         if (custom_flag === 'EIA') {
             cat = await this.categoryService.getCategorybyID(category_ID);
         };
-       
+
         //console.log(cat.childSeries)
         console.log(paginationDto)
         console.log(paginationDto.Frequency, paginationDto.Region)
-        let series 
+        let series
         if (custom_flag === 'EIA') {
             series = await this.seriesService.getManySeries(cat.category_id,
-            paginationDto.Frequency, paginationDto.Region);
+                paginationDto.Frequency, paginationDto.Region);
         } else if (custom_flag === 'custom') {
             series = await this.seriesService.getCustomSeries(category_ID, paginationDto.Frequency,
                 paginationDto.Region)
@@ -80,9 +81,9 @@ export class DownloadController {
         let workbook = new Excel.Workbook()
         let worksheet = workbook.addWorksheet('data');
         console.log('before name_cols')
-        let name_cols = [{header: 'Name', key: 'name', width: 60}, 
-                         {header: 'Region', key: 'region', width: 20},
-                         {header: 'Units', key: 'units', width: 20},]
+        let name_cols = [{ header: 'Name', key: 'name', width: 60 },
+        { header: 'Region', key: 'region', width: 20 },
+        { header: 'Units', key: 'units', width: 20 },]
         console.log('after name_cols')
         console.log(series[0])
         let year_data = series[0].data
@@ -92,12 +93,13 @@ export class DownloadController {
         }
         console.log('before year cols')
         let year_cols = year_data.map(year => {
-                    return {header: year[0], key: year[0], width: 10}})
-        
+            return { header: year[0], key: year[0], width: 10 }
+        })
+
         let cols = [...name_cols, ...year_cols]
         console.log('before set worksheet cols')
         console.log(cols);
-        
+
         let region = ''
         if (cat?.dataset_name && cat.dataset_name.includes('Annual Energy Outlook')) {
             region = 'USA'
@@ -106,39 +108,47 @@ export class DownloadController {
         let data_rows = series.map(series => {
             console.log('in outer map')
             const data_array = series.data.map(year => {
-                    return {[year[0]]: year[1]}})
+                return { [year[0]]: year[1] }
+            })
             if (Number(Object.keys(data_array[0])[0]) > Number(Object.keys(data_array[0])[0])) {
                 data_array.reverse()
             }
-      
-           
-           console.log('before data_object map') 
+
+
+            console.log('before data_object map')
             const data_object = Object.assign({}, ...(data_array.map(item =>
                 (item))));
-            console.log('after define data object')
-            console.log(series.name)
-            console.log(region)
-            console.log(series.geography)
-            console.log(region.length ? region : series.geography)
-            console.log(series.units)
-            let return_obj = {name: series.name, region: region.length ? region : series.geography, units: series.units, ...data_object}
+
+            let return_obj = { name: series.name, region: region.length ? region : series.geography, units: series.units, ...data_object }
             console.log('defined return obj')
             return return_obj
         })
         //console.log(data_rows)
         console.log('before add rows')
-        worksheet.addRows(data_rows)
+        console.log(data_rows)
+        // Add an array of rows
+        const rows = [
+            //[5, 'Bob', new Date()], // row by array
+            { id: 6, name: 'Barbara', dob: new Date() }
+        ];
+        // add new rows and return them as array of row objects
+        const newRows = worksheet.addRows(rows);
+        //worksheet.addRows(data_rows)
+
         console.log('after add rows')
-        worksheet.spliceRows(1, 0, ...new Array(5))
+        console.log(newRows)
+        //worksheet.spliceRows(1, 0, ...new Array(5))
         let name = cat?.name ? cat.name : 'US Electricity GDP';
-        let dataset_name = cat?.dataset_name ? cat.dataset_name : 'Custom';
-        worksheet.getRow(1).values = ["Data Group:", name]
-        worksheet.getRow(1).font = {size: 16}
-        worksheet.getRow(2).values = ["Data Set Name (top level category):",  dataset_name]
-        worksheet.getRow(2).font = {size: 16}
-        worksheet.getRow(3).values = ["EIA API:", "https://www.eia.gov/opendata/"]
-        worksheet.getRow(3).font = {size: 16}
-        worksheet.getRow(6).font = {bold: true}
+
+        // let dataset_name = cat?.dataset_name ? cat.dataset_name : 'Custom';
+        // worksheet.getRow(1).values = ["Data Group:", name]
+        // worksheet.getRow(1).font = {size: 16}
+        // worksheet.getRow(2).values = ["Data Set Name (top level category):",  dataset_name]
+        // worksheet.getRow(2).font = {size: 16}
+        // worksheet.getRow(3).values = ["EIA API:", "https://www.eia.gov/opendata/"]
+        // worksheet.getRow(3).font = {size: 16}
+        // worksheet.getRow(6).font = {bold: true}
+
         // res is a Stream object
         // response.setHeader(
         // "Content-Type",
@@ -156,6 +166,6 @@ export class DownloadController {
         // const xl = createReadStream('./src/test.xlsx')
         // response.send(xl)
     }
-    
+
 }
 
