1f59aaa7e857bb10bbf575c0226c85e9cc9f9f30
Zachary Schmidt <zacharym.schmidt@gmail.com>
2021-08-16 11:53:35 -0700

    reverted to using config module to test why it was breaking production server

--- server/src/auth/auth-header-api-key.strategy.ts
+++ server/src/auth/auth-header-api-key.strategy.ts
@@ -1,12 +1,12 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-//import { ConfigService } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
 import Strategy from 'passport-headerapikey';
 
 @Injectable()
 export class HeaderApiKeyStrategy extends PassportStrategy(Strategy, 'api-key') {
     constructor(
-        //private readonly configService: ConfigService
+        private readonly configService: ConfigService
     ) {
         super({ header: 'X-API-KEY', prefix: '' },
         true,
@@ -16,8 +16,7 @@ export class HeaderApiKeyStrategy extends PassportStrategy(Strategy, 'api-key') 
     }
 
     public validate = (apiKey: string, done: (error: Error, data) => {}) => {
-        //if (this.configService.get<string>('API_KEY') === apiKey) {
-          if (apiKey === '1db2c2d4e7f61gb2cjd4e5f6') {
+        if (this.configService.get<string>('API_KEY') === apiKey) {
             
             done(null, true);
         }
--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { HeaderApiKeyStrategy } from './auth-header-api-key.strategy';
-//import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PassportModule, 
-    //ConfigModule
-  ],
+  imports: [PassportModule, ConfigModule],
   providers: [HeaderApiKeyStrategy],
 })
 export class AuthModule {}
